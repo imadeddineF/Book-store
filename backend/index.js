269c7147b262ae54b0c488b306e1a0a49125c859
imadeddineF <imadeddinefillalipro@gmail.com>
2023-12-24 18:45:55 +0100

    /

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://imad-ed-fl:ufiesPI7sWeQacL9@cluster0.cyjtgvu.mongodb.net/book-store?retryWrites=true&w=majority"
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
 
 app.listen("3001", () => {
   console.log("hehe");
