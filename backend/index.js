05a0cb8d48fc0c3520d4a5fe916b173c6ac4cb1c
imadeddineF <imadeddinefillalipro@gmail.com>
2023-12-29 16:02:08 +0100

    GET and POST operations done

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,35 @@
+// Create server
 const express = require("express");
 const app = express();
+app.use(express.json());
+const cors = require("cors");
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Connect to DB
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb+srv://imad-ed-fl:ufiesPI7sWeQacL9@cluster0.cyjtgvu.mongodb.net/book-store?retryWrites=true&w=majority"
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello from server!");
+// Import book model
+const BookModel = require("./models/Books");
+
+// GET
+app.get("/books", async (req, res) => {
+  const books = await BookModel.find();
+  res.json(books);
+});
+
+// POST
+app.post("/createBook", async (req, res) => {
+  const newBook = new BookModel(req.body);
+  await newBook.save();
+
+  res.json(newBook);
 });
 
 app.listen("3001", () => {
-  console.log("hehe");
+  console.log("testing");
 });
--- backend/models/Books.js
+++ backend/models/Books.js
@@ -1,1 +1,10 @@
-const app = express();
+const mongoose = require("mongoose");
+
+const bookSchema = mongoose.Schema({
+  name: String,
+  author: String,
+});
+
+const bookModel = mongoose.model("book", bookSchema);
+
+module.exports = bookModel;
